8f4c333e3fa594270f8f7ae4f8030c702a020836
zgcarvalho <zgcarvalho@outlook.com>
2022-02-15 14:27:44 -0300

    jumbotron style

--- KVshiny/R/mod_ui_jumbotron.R
+++ KVshiny/R/mod_ui_jumbotron.R
@@ -1,15 +1,31 @@
-#' Create jumbotron of main Run KVFinder page 
-#' 
+#' Create jumbotron of main Run KVFinder page
+#'
 #' @import shiny
-#' 
+#'
 
-create_jumbotron <- function(){
-  jumb <- tags$div(class = "jumbotron bg-primary", style = "max-height: 400px;height:56vh;",
-           h1(class = "display-7", "Welcome to the KVFinder-web service!", style = "margin-top:-5%;margin-bottom:0%"),
-           img(class ="col-sm-8" ,src="https://lnbio.cnpem.br/wp-content/uploads/2022/01/space-segmentation.png"),
-           p(class = 'lead', "A web service for cavity detection and characterization in any type of biomolecular structure"),
-           hr(class = "my-1"),
-           tags$button(class = "btn btn-default action-button", id = "more_button", type = "button", "More", style = "background-color: #6c757d; color:white")  
-  )
+create_jumbotron <- function() {
+  jumb <-
+    tags$div(
+      class = "jumbotron bg-primary",
+      style = "padding: 1rem 2rem;",
+      h1(
+        class = "display-7",
+        "Welcome to the KVFinder-web service!",
+        style = "margin-left: .5rem;"
+      ),
+      img(class = "col-sm-8" , src = "https://lnbio.cnpem.br/wp-content/uploads/2022/01/space-segmentation.png"),
+      h5(
+        "A web service for cavity detection and characterization in any type of biomolecular structure",
+        style = "margin: 1rem;"
+      ),
+      hr(class = "my-1"),
+      tags$button(
+        class = "btn btn-default action-button",
+        id = "more_button",
+        type = "button",
+        "More",
+        style = "background-color: #6c757d; color:white"
+      )
+    )
   return(jumb)
 }
