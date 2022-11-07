257d421137eda6132de3dfd2dfc8f320570ec65b
Helder Ribeiro <helder.ribeiro@lnbio.cnpem.br>
2022-11-07 12:01:49 -0300

    small changes text

--- KVshiny/R/mod_kvTutorial.R
+++ KVshiny/R/mod_kvTutorial.R
@@ -39,7 +39,7 @@ kv_tutorial <- function() {
     ### Step 1. Load biomolecular structure from PDB
     tags$p(
       "First of all, you must open ",
-      strong("Run KVFinder"),
+      strong("Run Cavity Analysis"),
       " tab to start your cavity analysis pipeline.",
       style = "text-align:justify"
     ),
@@ -51,7 +51,7 @@ kv_tutorial <- function() {
       strong("Type of input"),
       "must be set to 'Fetch from PDB'. Then, you must type '1PRG' in the ",
       strong("PDB ID"),
-      "text box. Finally, click on ",
+      "text box. Finally, click on the",
       strong("Load"),
       "button."
     ),
@@ -61,7 +61,7 @@ kv_tutorial <- function() {
     ),
     tags$br(),
     tags$p(
-      "Upon successfully loading the structure from PDB database, an option to keep non-standard residues (e.g., water, ligands, ions, etc) will appear in the screen. For 1PRG, we do not want to keep water molecules for further analysis. So, we do not check 'HOH' checkbox."
+      "Upon successfully loading the structure from PDB database, an option to keep non-standard residues (e.g., water, ligands, ions, etc) will appear on the screen. For 1PRG, we want to avoid keeping water molecules for further analysis. So, we do not check 'HOH' on the checkbox."
     ),
     tags$br(),
     fluidRow(
@@ -71,14 +71,14 @@ kv_tutorial <- function() {
 
     ### Step 2. Run Cavity Analysis
     tags$p(
-      "Now, we can explore the four different execution modes, that are:",
+      "Now, we can explore the four different execution modes, which are:",
       style = "text-align:justify"
     ),
     tags$ol(
       #### Whole protein (default)
       tags$li(strong("Whole structure with default parameters")),
       tags$p(
-        "Here, you must select 'Whole structure (default)' into the ",
+        "Here, you must select 'Whole structure (default)' in the ",
         strong("Step 2. Choose run mode"),
         ". The default parameters are designed to make a simple and fast whole structure detection."
       ),
@@ -103,9 +103,9 @@ kv_tutorial <- function() {
       tags$p(
         "After successfully submitting the job to KVFinder-web service, a window will appear with the",
         strong("Job ID"),
-        ". For instace, the Job ID of our submission is",
+        ". For instance, the Job ID of our submission is",
         em("6524504029010759880"),
-        ". To check of you submission, click on ",
+        ". To check your submission, click on ",
         strong("Check Results"),
         "button."
       ),
@@ -119,7 +119,7 @@ kv_tutorial <- function() {
         strong("Status"),
         "field will change to",
         strong("completed"),
-        "and turn green. Then, three button and a table with cavity names, volumes and areas will appear on the screen."
+        "and turn green. Then, three buttons and a table with cavity names, volumes and areas will appear on the screen."
       ),
       fluidRow(
         column(12, align = "center", tags$img(src = "www/tutorial-step3-wp-mode1-check-results.png", align = "center")),
@@ -149,7 +149,7 @@ kv_tutorial <- function() {
           ###### 3
           tags$li(
             strong("View:"),
-            "Enable a interactive window to visualize the cavities with the target biomolecular structure."
+            "Enable an interactive window to visualize the cavities with the target biomolecular structure."
           ),
           fluidRow(
             column(12, align = "center", tags$img(src = "www/tutorial-step3-wp-mode1-view.png", align = "center")),
@@ -169,7 +169,7 @@ kv_tutorial <- function() {
       tags$p(
         "Still, we can show the residues surrounding the selected cavities by clicking on",
         strong("Show interface residues"),
-        "checkbutton."
+        "check button."
       ),
       fluidRow(
         column(12, align = "center", tags$img(src = "www/tutorial-step3-wp-mode1-view3.png", align = "center")),
@@ -185,14 +185,14 @@ kv_tutorial <- function() {
         "."
       ),
       tags$p(
-        "KVFinder-web service is all about parameters customization of parKVFinder software. The detection parameters and their impact on cavity detection are explained in details in the",
+        "KVFinder-web service is all about parameters customization of parKVFinder software. The detection parameters and their impact on cavity detection are explained in detail in the",
         strong("Help"),
-        " tab. In summary, 'Probe In' defines the inner limits of the cavities, 'Probe Out' defines outer limits of the cavities, 'Removal Distance' defines the length to be remove from the cavity-bulk boundary and 'Volume Cutoff' excludes cavities with volumes smaller than this limit. Let's show the effect of varying 'Volume Cutoff', 'Probe Out' and 'Removal Distance'."
+        " tab. In summary, 'Probe In' defines the inner limits of the cavities, 'Probe Out' defines the outer limits of the cavities, 'Removal Distance' defines the length to be removed from the cavity-bulk boundary and 'Volume Cutoff' excludes cavities with volumes smaller than this limit. Let's show the effect of varying 'Volume Cutoff', 'Probe Out' and 'Removal Distance'."
       ),
       tags$br(),
       tags$ul(
         ##### Adjusting Volume Cutoff
-        tags$li(strong("Adjusting Volume Cutoff:"), "the Volume Cutoff limits the smaller cavity that would be identified. So, we set Volume Cutoff to 500 Å³. Them, we submit the job to KVFinder-web service again."),
+        tags$li(strong("Adjusting Volume Cutoff:"), "the Volume Cutoff limits the smaller cavity that would be identified. So, we set Volume Cutoff to 500 Å³. Then, we submit the job to KVFinder-web service again."),
         tags$p(
           "After completion, display the job results by clicking on ",
           strong("View"), "button."
@@ -201,7 +201,7 @@ kv_tutorial <- function() {
           column(12, align = "center", tags$img(src = "www/tutorial-step3-wp-mode2-vc500.png", align = "center")),
         ),
         tags$br(),
-        tags$p("With a greater Volume Cutoff, we identified less cavities compared to the detection with default parameters."),
+        tags$p("With a greater Volume Cutoff, we identified fewer cavities compared to the detection with default parameters."),
         tags$br(),
 
         ##### Adjusting Probe Out
@@ -214,7 +214,7 @@ kv_tutorial <- function() {
           column(12, align = "center", tags$img(src = "www/tutorial-step3-wp-mode2-po8-vc500.png", align = "center")),
         ),
         tags$br(),
-        tags$p("With a larger Probe Out, the detected cavities has a higher boundary than that detected with the 4.0 Å probe. Therefore, by increasing the size of the Probe Out, the cavity boundary is also raised."),
+        tags$p("With a larger Probe Out, the detected cavities have a higher boundary than that detected with the 4.0 Å probe. Therefore, by increasing the size of the Probe Out, the cavity boundary is also raised."),
         tags$br(),
 
         ##### Adjusting Removal Distance
@@ -227,7 +227,7 @@ kv_tutorial <- function() {
           column(12, align = "center", tags$img(src = "www/tutorial-step3-wp-mode2-rd12-vc500.png", align = "center")),
         ),
         tags$br(),
-        tags$p("With a smaller Removal Distance, the detected cavities has a higher boundary than that detected with the 2.4 Å. Therefore, by decreasing the Removal Distance, the cavity boundary is also raised."),
+        tags$p("With a smaller Removal Distance, the detected cavities have a higher boundary than that detected with the 2.4 Å. Therefore, by decreasing the Removal Distance, the cavity boundary is also raised."),
         tags$p("Therefore, changing the cavity boundary by varying Probe Out and Removal Distance also affects cavity segregation."),
         tags$br(),
       ),
@@ -236,9 +236,9 @@ kv_tutorial <- function() {
 
       #### Steered detection
       tags$p(
-        "Another important feature of parKVFinder is the steered detection of cavities. Here, instead of analyzing the PPAR-gamma structure, we are analyzing HIV-1 protease (PDB ID: 1HVR). First, we have to replicate",
+        "Another important feature of the parKVFinder is the steered detection of cavities. Here, instead of analyzing the PPAR-gamma structure, we are analyzing HIV-1 protease (PDB ID: 1HVR). First, we have to replicate",
         strong("Step 1"),
-        "by replacing '1PRG' for '1HVR' and click on ",
+        "by replacing '1PRG' with '1HVR' and click on ",
         strong("Load"),
         "button again."
       ),
@@ -308,7 +308,7 @@ kv_tutorial <- function() {
     ## Submitting an already sent job description
     tags$h5(strong("Submitting an already sent job")),
     tags$p(
-      "Users may submit a job that have already been submitted to the KVFinder-web service and the job is still available on the web service."
+      "Users may submit a job that has already been submitted to the KVFinder-web service and the job is still available on the web service."
     ),
     tags$p(
       "In this scenario, the",
@@ -327,7 +327,7 @@ kv_tutorial <- function() {
     tags$p("Users may share their jobs with colleagues by the Job ID that they receive when submitting a job."),
     tags$p(
       "First, you must open ",
-      strong("Get latest results"),
+      strong("Retrive results"),
       ". After, you must fill a Job ID (e.g.,",
       em("6524504029010759880"),
       "- ID of our first job in this tutorial) in the ",
@@ -345,7 +345,7 @@ kv_tutorial <- function() {
     tags$p(
       "If the job was available in the KVFinder-web service, a field will appear with the Job ID Status. After completion, the",
       strong("Status"),
-      "field will change to completed and turn green. Then, three button and a table with cavity names, volumes and areas will appear on the screen. Finally, we can display the job results by clicking on View button."
+      "field will change to completed and turn green. Then, three buttons and a table with cavity names, volumes and areas will appear on the screen. Finally, we can display the job results by clicking on View button."
     ),
     fluidRow(
       column(12, align = "center", tags$img(src = "www/tutorial-retrieve-job-results.png", align = "center")),
@@ -353,7 +353,7 @@ kv_tutorial <- function() {
     tags$br(),
 
     ### Job does not exist
-    tags$p("Otherwise, the job does not exist or is not available in the KVFinder-web service anymore. In both situation, a error message will appear in the Job status field."),
+    tags$p("Otherwise, the job does not exist or is not available in the KVFinder-web service anymore. In both situations, an error message will appear in the Job status field."),
     fluidRow(
       column(12, align = "center", tags$img(src = "www/tutorial-retrieve-job-bad-id.png", align = "center")),
     ),
