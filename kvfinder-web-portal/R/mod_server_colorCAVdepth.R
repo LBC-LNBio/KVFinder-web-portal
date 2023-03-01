53c60249a817532c123be830b1e66a483e14dea7
Helder Ribeiro <helder.ribeiro@lnbio.cnpem.br>
2023-03-01 13:23:59 -0500

    Update cavity selection (#27)
    * improve depth and hyd relationship with cav selected

--- kvfinder-web-portal/R/mod_server_colorCAVdepth.R
+++ kvfinder-web-portal/R/mod_server_colorCAVdepth.R
@@ -16,9 +16,11 @@
 color_cavity_deepth <- function(input, output, is_pg2, cav_rep_list, result_pdb_list) {
   if (is_pg2 == TRUE) {
     input_cavity_deep <- "input_cavity_deep_pg2"
+    select_cavity <- "select_cavity_pg2"
     structure <- "structure_pg2"
   } else {
     input_cavity_deep <- "input_cavity_deep"
+    select_cavity <- "select_cavity"
     structure <- "structure"
   }
   #print(result_pdb_list$result_toml$MAX_DEPTH)
@@ -28,6 +30,7 @@ color_cavity_deepth <- function(input, output, is_pg2, cav_rep_list, result_pdb_
   # After the initial structure is invisible, we can add a new representation to the current scene
   
   if(input[[input_cavity_deep]] == TRUE){
+    if(input[[select_cavity]] == "All"){
     NGLVieweR_proxy(structure) %>%
       addSelection("point",
                    param =
@@ -41,6 +44,21 @@ color_cavity_deepth <- function(input, output, is_pg2, cav_rep_list, result_pdb_
                        #colorScheme = scheme_color_list[[tail(protein_col_scheme_list, n = 1)]]
                      )
       )
+  } else {
+    NGLVieweR_proxy(structure) %>%
+      addSelection("point",
+                   param =
+                     list(
+                       name = "deepth", # now the created selection is named "sel3"
+                       sele = input[[select_cavity]],
+                       colorScheme = 'bfactor',
+                       colorScale = 'rainbow',
+                       colorReverse = TRUE,
+                       colorDomain = c(0.0, max(unlist(result_pdb_list$result_toml$MAX_DEPTH)))
+                       #colorScheme = scheme_color_list[[tail(protein_col_scheme_list, n = 1)]]
+                     )
+      )
+  }
   } else{
     NGLVieweR_proxy(structure) %>%
        removeSelection(name = "deepth")
