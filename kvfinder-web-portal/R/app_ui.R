6fb2bd3bbacadab910e6a713dfc4c20303d9663b
jvsguerra <jvsguerra@gmail.com>
2023-09-06 19:05:43 -0300

    conditionalPanel for Submit, Check Results and View buttons in Run Cavity Analysis tab and conditionalPanel for Get Results and View buttons in Retrieve Results page

--- kvfinder-web-portal/R/app_ui.R
+++ kvfinder-web-portal/R/app_ui.R
@@ -12,6 +12,8 @@
 #' @noRd
 #'
 
+
+
 app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
@@ -57,7 +59,7 @@ app_ui <- function(request) {
             icon = icon("angle-double-right")
           ),
           menuItem(
-            "Retrieve results",
+            "Retrieve Results",
             tabName = "check_kv_sidebar",
             icon = icon("angle-double-right")
           ),
@@ -126,7 +128,7 @@ app_ui <- function(request) {
             #--------------------------------------------
             # Result section
             conditionalPanel(
-              condition = "input.submit_button > 0",
+              condition = "input.go_to_check_results > 0",
               tags$hr(),
               fluidRow(
                 column(
@@ -154,57 +156,60 @@ app_ui <- function(request) {
                 ),
                 column(
                   7,
-                  # function to fulscreen the entire div that contains the NGL viewer and all the othe buttons
-                  fullscreen_this(tags$div(
-                    id = "view_panel",
-                    fluidRow(
-                      column(
-                        12,
-                        NGLVieweROutput("structure", width = "100%", height = "75vh")
-                      )
-                    ),
-                    conditionalPanel(
-                      condition = "input.input_cavity_hyd==1",
+                  conditionalPanel(
+                    condition = "input.view_str > 0",
+                    # function to fulscreen the entire div that contains the NGL viewer and all the othe buttons
+                    fullscreen_this(tags$div(
+                      id = "view_panel",
                       fluidRow(
-                        column(12,
-                          align = "center",
-                          uiOutput(outputId = "scale_plot")
+                        column(
+                          12,
+                          NGLVieweROutput("structure", width = "100%", height = "75vh")
                         )
-                      )
-                    ),
-                    conditionalPanel(
-                      condition = "input.input_cavity_deep==1",
-                      fluidRow(
-                        column(12,
-                          align = "center",
-                          plotOutput("scale_plot_deep", height = 60, width = "60%"),
-                          tags$head(
-                            tags$style(
-                              HTML(
-                                "#scale_plot_deep { max-width: 600px; align: center; }"
+                      ),
+                      conditionalPanel(
+                        condition = "input.input_cavity_hyd==1",
+                        fluidRow(
+                          column(12,
+                            align = "center",
+                            uiOutput(outputId = "scale_plot")
+                          )
+                        )
+                      ),
+                      conditionalPanel(
+                        condition = "input.input_cavity_deep==1",
+                        fluidRow(
+                          column(12,
+                            align = "center",
+                            plotOutput("scale_plot_deep", height = 60, width = "60%"),
+                            tags$head(
+                              tags$style(
+                                HTML(
+                                  "#scale_plot_deep { max-width: 600px; align: center; }"
+                                )
                               )
                             )
                           )
                         )
-                      )
-                    ),
-                    fluidRow(
-                      column(2, uiOutput("selection_pdb")),
-                      column(2, uiOutput("cavity_rep")),
-                      column(2, uiOutput("cavity_color")),
-                      column(2, div(style = "display: inline-block; vertical-align: -20px;", uiOutput("cavity_deep"))),
-                      column(2, div(style = "display: inline-block; vertical-align: -20px;", uiOutput("cavity_hyd"))),
-                      column(2, div(style = "display: inline-block; vertical-align: -20px;", uiOutput("show_interface"))),
-                    ),
-                    fluidRow(
-                      column(2, uiOutput("protein_rep")),
-                      column(2, uiOutput("protein_color")),
-                      column(3, uiOutput("protein_color_scheme")),
-                      column(2, uiOutput("bg_color")),
-                      column(2, align = "center", uiOutput("snapshot_title"), uiOutput("snapshot")),
-                      column(1, align = "center", uiOutput("fullscreen_title"), uiOutput("fullscreen"))
-                    ),
-                  ), click_id = "fullscreen")
+                      ),
+                      fluidRow(
+                        column(2, uiOutput("selection_pdb")),
+                        column(2, uiOutput("cavity_rep")),
+                        column(2, uiOutput("cavity_color")),
+                        column(2, div(style = "display: inline-block; vertical-align: -20px;", uiOutput("cavity_deep"))),
+                        column(2, div(style = "display: inline-block; vertical-align: -20px;", uiOutput("cavity_hyd"))),
+                        column(2, div(style = "display: inline-block; vertical-align: -20px;", uiOutput("show_interface"))),
+                      ),
+                      fluidRow(
+                        column(2, uiOutput("protein_rep")),
+                        column(2, uiOutput("protein_color")),
+                        column(3, uiOutput("protein_color_scheme")),
+                        column(2, uiOutput("bg_color")),
+                        column(2, align = "center", uiOutput("snapshot_title"), uiOutput("snapshot")),
+                        column(1, align = "center", uiOutput("fullscreen_title"), uiOutput("fullscreen"))
+                      ),
+                    ), click_id = "fullscreen")
+                  )
                 )
               )
             ),
@@ -236,84 +241,90 @@ app_ui <- function(request) {
                     label = "Get results",
                     icon = icon("share-square")
                   ),
-                  tags$br(),
-                  tags$br(),
-                  uiOutput("output_status_pg2"),
-                  fluidRow(
-                    column(
-                      4,
-                      uiOutput("download_pg2")
-                    ),
-                    column(
-                      4,
-                      uiOutput("download2_pg2")
-                    ),
-                    column(
-                      4,
-                      uiOutput(
-                        "view_output_pg2"
+                  conditionalPanel(
+                    condition = "input.check_loc_pg2 > 0",
+                    tags$br(),
+                    tags$br(),
+                    uiOutput("output_status_pg2"),
+                    fluidRow(
+                      column(
+                        4,
+                        uiOutput("download_pg2")
+                      ),
+                      column(
+                        4,
+                        uiOutput("download2_pg2")
+                      ),
+                      column(
+                        4,
+                        uiOutput(
+                          "view_output_pg2"
+                        )
                       )
+                    ),
+                    tags$br(),
+                    uiOutput("results_table_pg2"),
+                    tags$br(),
+                    sidebar = boxSidebar(
+                      id = "help_get_results_pg2",
+                      icon = icon("info"),
+                      p("Here, you can insert a job ID to get or check your latest results.")
                     )
-                  ),
-                  tags$br(),
-                  uiOutput("results_table_pg2"),
-                  tags$br(),
-                  sidebar = boxSidebar(
-                    id = "help_get_results_pg2",
-                    icon = icon("info"),
-                    p("Here, you can insert a job ID to get or check your latest results.")
                   )
                 )
               ),
               column(
                 7,
-                fullscreen_this(tags$div(
-                  id = "view_panel_pg2",
-                  fluidRow(
-                    NGLVieweROutput("structure_pg2", width = "100%", height = "600px")
-                  ),
-                  conditionalPanel(
-                    condition = "input.input_cavity_hyd_pg2==1",
+                conditionalPanel(
+                  condition = "input.view_str_pg2 > 0",
+                  fullscreen_this(tags$div(
+                    id = "view_panel_pg2",
                     fluidRow(
-                      column(12,
-                        align = "center",
-                        uiOutput(outputId = "scale_plot_pg2")
+                      NGLVieweROutput("structure_pg2", width = "100%", height = "600px")
+                    ),
+                    conditionalPanel(
+                      condition = "input.input_cavity_hyd_pg2==1",
+                      fluidRow(
+                        column(12,
+                          align = "center",
+                          uiOutput(outputId = "scale_plot_pg2")
+                        )
                       )
-                    )
-                  ),
-                  conditionalPanel(
-                    condition = "input.input_cavity_deep_pg2==1",
-                    fluidRow(
-                      column(12,
-                        align = "center",
-                        plotOutput("scale_plot_deep_pg2", height = 60, width = "60%"),
-                        tags$head(
-                          tags$style(
-                            HTML(
-                              "#scale_plot_deep_pg2 { max-width: 600px; align: center; }"
+                    ),
+                    conditionalPanel(
+                      condition = "input.input_cavity_deep_pg2==1",
+                      fluidRow(
+                        column(12,
+                          align = "center",
+                          plotOutput("scale_plot_deep_pg2", height = 60, width = "60%"),
+                          tags$head(
+                            tags$style(
+                              HTML(
+                                "#scale_plot_deep_pg2 { max-width: 600px; align: center; }"
+                              )
                             )
                           )
                         )
                       )
+                    ),
+                    fluidRow(
+                      column(2, uiOutput("selection_pdb_pg2")),
+                      column(2, uiOutput("cavity_rep_pg2")),
+                      column(2, uiOutput("cavity_color_pg2")),
+                      column(2, div(style = "display: inline-block; vertical-align: -20px;", uiOutput("cavity_deep_pg2"))),
+                      column(2, div(style = "display: inline-block; vertical-align: -20px;", uiOutput("cavity_hyd_pg2"))),
+                      column(2, div(style = "display: inline-block; vertical-align: -20px;", uiOutput("show_interface_pg2"))),
+                    ),
+                    fluidRow(
+                      column(2, uiOutput("protein_rep_pg2")),
+                      column(2, uiOutput("protein_color_pg2")),
+                      column(3, uiOutput("protein_color_scheme_pg2")),
+                      column(2, uiOutput("bg_color_pg2")),
+                      column(2, align = "center", uiOutput("snapshot_title_pg2"), uiOutput("snapshot_pg2")),
+                      column(1, align = "center", uiOutput("fullscreen_title_pg2"), uiOutput("fullscreen_pg2"))
                     )
-                  ),
-                  fluidRow(
-                    column(2, uiOutput("selection_pdb_pg2")),
-                    column(2, uiOutput("cavity_rep_pg2")),
-                    column(2, uiOutput("cavity_color_pg2")),
-                    column(2, div(style = "display: inline-block; vertical-align: -20px;", uiOutput("cavity_deep_pg2"))),
-                    column(2, div(style = "display: inline-block; vertical-align: -20px;", uiOutput("cavity_hyd_pg2"))),
-                    column(2, div(style = "display: inline-block; vertical-align: -20px;", uiOutput("show_interface_pg2"))),
-                  ),
-                  fluidRow(
-                    column(2, uiOutput("protein_rep_pg2")),
-                    column(2, uiOutput("protein_color_pg2")),
-                    column(3, uiOutput("protein_color_scheme_pg2")),
-                    column(2, uiOutput("bg_color_pg2")),
-                    column(2, align = "center", uiOutput("snapshot_title_pg2"), uiOutput("snapshot_pg2")),
-                    column(1, align = "center", uiOutput("fullscreen_title_pg2"), uiOutput("fullscreen_pg2"))
-                  )
-                ), click_id = "fullscreen_pg2")
+                  ), click_id = "fullscreen_pg2")
+                )
               )
             )
           ),
@@ -360,30 +371,5 @@ app_ui <- function(request) {
           "<!-- Cloudflare Web Analytics --><script defer src=\"https://static.cloudflareinsights.com/beacon.min.js\" data-cf-beacon=\"{\"token\": \"3885c958deb74bfcbdfe31b04ae5e67f\"}\"></script><!-- End Cloudflare Web Analytics -->"
         )
     ),
-  )
-}
-
-#' Add external Resources to the Application
-#'
-#' This function is internally used to add external
-#' resources inside the Shiny application.
-#'
-#' @import shiny
-#' @importFrom golem add_resource_path activate_js favicon bundle_resources
-#' @noRd
-#'
-#'
-
-golem_add_external_resources <- function() {
-  add_resource_path("www", app_sys("app/www"))
-  tags$head(
-    tags$link(
-      rel = "shortcut icon",
-      href = "www/kvfinder-web.ico"
-    ),
-    bundle_resources(
-      path = app_sys("app/www"),
-      app_title = "KVFinder-web"
-    )
   )
 }
